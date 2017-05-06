50ce91cb4cd630bf757d7bc1e2d5297499aab9fc
Adam Wilson <adamw@ellipticbit.com>
2017-05-06 15:15:41 +0200

    Improved examples.

--- source/hash.d
+++ source/hash.d
@@ -66,14 +66,17 @@ unittest {
 	ubyte[] vec1 = hash(cast(ubyte[])"");
 	ubyte[] vec2 = hash(cast(ubyte[])"abc");
 	ubyte[] vec3 = hash(cast(ubyte[])"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq");
+	ubyte[] vec4 = hash(cast(ubyte[])"The quick brown fox jumps over the lazy dog.");
 
 	writeln(toHexString!(LetterCase.lower)(vec1));
 	writeln(toHexString!(LetterCase.lower)(vec2));
 	writeln(toHexString!(LetterCase.lower)(vec3));
+	writeln(toHexString!(LetterCase.lower)(vec4));
 
 	assert(toHexString!(LetterCase.lower)(vec1) == "38b060a751ac96384cd9327eb1b1e36a21fdb71114be07434c0cc7bf63f6e1da274edebfe76f65fbd51ad2f14898b95b");
 	assert(toHexString!(LetterCase.lower)(vec2) == "cb00753f45a35e8bb5a03d699ac65007272c32ab0eded1631a8b605a43ff5bed8086072ba1e7cc2358baeca134c825a7");
 	assert(toHexString!(LetterCase.lower)(vec3) == "3391fdddfc8dc7393707a65b1b4709397cf8b1d162af05abfe8f450de5f36bc6b0455a8520bc4e6f5fe95b1fe3c8452b");
+	assert(toHexString!(LetterCase.lower)(vec4) == "ed892481d8272ca6df370bf706e4d7bc1b5739fa2177aae6c50e946678718fc67a7af2819a021c2fc34e91bdb63409d7");
 }
 
 @trusted public ubyte[] hash(string path)
