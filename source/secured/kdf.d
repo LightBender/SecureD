31fdfbe43e9127d89d814d0dd9bf33f6100c86e4
Adam Wilson <adamw@ellipticbit.com>
2018-05-28 01:21:44 -0700

    Added support for Botan and HKDF Info parameter.

--- source/secured/kdf.d
+++ source/secured/kdf.d
@@ -176,44 +176,69 @@ unittest
 @safe public KdfResult hkdf(ubyte[] key, ulong outputLen) {
     KdfResult result;
     result.salt = random(getHashLength(HashFunction.SHA2_384));
-    result.key = hkdf_ex(key, result.salt, outputLen, HashFunction.SHA2_384);
+    result.key = hkdf_ex(key, result.salt, string.init, outputLen, HashFunction.SHA2_384);
     return result;
 }
 
-@trusted public ubyte[] hkdf_ex(ubyte[] key, ubyte[] salt, ulong outputLen, HashFunction func) {
+@trusted public ubyte[] hkdf_ex(ubyte[] key, ubyte[] salt, string info, ulong outputLen, HashFunction func) {
     if (key.length == 0) {
         throw new CryptographicException("HKDF key cannot be an empty array.");
     }
 
-    EVP_PKEY_CTX* pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_HKDF, null);
-    scope(exit) {
-        if(pctx !is null) {
-            EVP_PKEY_CTX_free(pctx);
+    version(OpenSSL)
+    {
+        EVP_PKEY_CTX* pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_HKDF, null);
+        scope(exit) {
+            if(pctx !is null) {
+                EVP_PKEY_CTX_free(pctx);
+            }
         }
+
+        if (EVP_PKEY_derive_init(pctx) <= 0) {
+            throw new CryptographicException("Unable to create HKDF function.");
+        }
+        
+        if (EVP_PKEY_CTX_set_hkdf_md(pctx, getOpenSSLHashFunction(func)) <= 0) {
+            throw new CryptographicException("Unable to create HKDF hash function.");
+        }
+
+        if (salt.length != 0 && EVP_PKEY_CTX_set1_hkdf_salt(pctx, salt) <= 0) {
+            throw new CryptographicException("Unable to set HKDF salt.");
+        }
+
+        if (info.length != 0 && EVP_PKEY_CTX_add1_hkdf_info(pctx, info) <= 0) {
+            throw new CryptographicException("Unable to set HKDF info.");
+        }
+
+        if (EVP_PKEY_CTX_set1_hkdf_key(pctx, key) <= 0) {
+            throw new CryptographicException("Unable to set HKDF key.");
+        }
+
+        ubyte[] keyMaterial = new ubyte[outputLen];
+        if (EVP_PKEY_derive(pctx, keyMaterial.ptr, &outputLen) <= 0) {
+            throw new CryptographicException("Unable to generate the requested key material.");
+        }
+
+        return keyMaterial;
     }
 
-    if (EVP_PKEY_derive_init(pctx) <= 0) {
-        throw new CryptographicException("Unable to create HKDF function.");
-    }
-    
-    if (EVP_PKEY_CTX_set_hkdf_md(pctx, getOpenSSLHashFunction(func)) <= 0) {
-        throw new CryptographicException("Unable to create HKDF hash function.");
-    }
+    version(Botan)
+    {
+        if (info.length == 0) {
+            throw new CryptographicException("HKDF info cannot be an empty array.");
+        }
 
-    if (salt.length != 0 && EVP_PKEY_CTX_set1_hkdf_salt(pctx, salt) <= 0) {
-        throw new CryptographicException("Unable to set HKDF salt.");
+        auto hkdf = HKDF(new HMAC(getBotanHashFunction(func)), new HMAC(getBotanHashFunction(func)));
+        
+        hkdf.startExtract(salt.ptr, salt.length);
+        hkdf.extract(key.ptr, key.length);
+        hkdf.finishExtract();
+        
+        byte[] infoBytes = cast(ubyte[])info;
+        SecureVector!ubyte result = SecureVector!ubyte(outputLen);
+        hkdf.expand(result.ptr, result.length, infoBytes.ptr, infoBytes.length);
+        return result;
     }
-
-    if (EVP_PKEY_CTX_set1_hkdf_key(pctx, key) <= 0) {
-        throw new CryptographicException("Unable to set HKDF key.");
-    }
-
-    ubyte[] keyMaterial = new ubyte[outputLen];
-    if (EVP_PKEY_derive(pctx, keyMaterial.ptr, &outputLen) <= 0) {
-        throw new CryptographicException("Unable to generate the requested key material.");
-    }
-
-    return keyMaterial;
 }
 
 unittest
@@ -227,12 +252,12 @@ unittest
                        0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF,
                        0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF ];
 
-    ubyte[] vec2 = hkdf_ex(cast(ubyte[])"abc", salt, 64, HashFunction.SHA2_224);
-    ubyte[] vec3 = hkdf_ex(cast(ubyte[])"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq", salt, 64, HashFunction.SHA2_224);
+    ubyte[] vec2 = hkdf_ex(cast(ubyte[])"abc", salt, "", 64, HashFunction.SHA2_224);
+    ubyte[] vec3 = hkdf_ex(cast(ubyte[])"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq", salt, "test", 64, HashFunction.SHA2_224);
 
     writeln(toHexString!(LetterCase.lower)(vec2));
     writeln(toHexString!(LetterCase.lower)(vec3));
 
     assert(toHexString!(LetterCase.lower)(vec2) == "0fcc4d227bb180f4a2631da9bf158203ced36a73752d1f6fb05be764cbd13460556e6ddd69b0d3b2cdf08457a18253811e38d8059177e5dc22b5b52a6b1cb30a");
-    assert(toHexString!(LetterCase.lower)(vec3) == "c2bf93ca7706228a03f8362d2b6f726030db408319172dfab0da4ea496d9bb5d380d6ff0bddafec45ef2307a9a5980b9fb7c7c405993e4145534c5ae1521e996");
+    assert(toHexString!(LetterCase.lower)(vec3) == "d05e4ba15e07095b8b6dc3abbdde3f790fb4c1d6146e93e12312fbf54b5a1aff4c9c9108046fc390f2bef5fbcbf44d57ac05732525ccbf0a856821fe178f47c2");
 }
